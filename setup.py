103fbad266b890ed8f5f7886a218568fbc1ae539
kanyukabogdan <139249181+kanyukabogdan@users.noreply.github.com>
2023-08-08 20:46:58 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Level1 - 1.json',
+    version='',
+    packages=['sfx', 'traits', 'classes', 'entities'],
+    url='',
+    license='',
+    author='user',
+    author_email='',
+    description=''
+)
